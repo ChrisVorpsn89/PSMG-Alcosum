cc5833f5e1257bac593d45b8cf810399a778ab0a
TJ <thomas.jaenich@stud.uni-regensburg.de>
2017-05-19 11:38:00 +0200

    Color in GeoJSON

--- resources/js/geoTest.js
+++ resources/js/geoTest.js
@@ -1,12 +1,23 @@
-var vectorSource = new ol.source.Vector({
+var countrySource = new ol.source.Vector({
     projection : 'EPSG:3857',
     url: 'data/countries.geo.json',
     format: new ol.format.GeoJSON()
 
 });
-var vectorLayer = new ol.layer.Vector({
-  source: vectorSource
+var countryStyle = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: [203, 194, 185, 1]
+  }),
+  stroke: new ol.style.Stroke({
+    color: [177, 163, 148, 0.5],
+    width: 2
+  })
 });
+var countryLayer = new ol.layer.Vector({
+  source: countrySource,
+    style: countryStyle
+});
+
 var center = ol.proj.transform([0, 0], 'EPSG:4326', 'EPSG:3857');
 var view = new ol.View ({
   center: center,
@@ -14,6 +25,6 @@ var view = new ol.View ({
 });
 var map = new ol.Map({
  target: 'map',
-  layers: [vectorLayer],
+  layers: [countryLayer],
   view: view
 });
