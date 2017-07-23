ce041d6deecd5684150b016fcdd5dfff39db475d
ChrisVorpsn89 <CVorpahl@gmx.de>
2017-07-23 13:10:09 +0200

    timeSlider Bug fixed

--- resources/js/timeSlider.js
+++ resources/js/timeSlider.js
@@ -1,6 +1,13 @@
-var slider = new Slider("#timeslider");
-
+/**var slider = new Slider("#timeslider");
 slider.on("slide", function(sliderValue) {
 	document.getElementById("timeSliderVal").textContent = sliderValue;
+	console.log(sliderValue);
 	getYear(sliderValue);
+});**/
+
+var slider = new Slider('#timeslider', {
+	formatter: function(value) {
+		document.getElementById("timeSliderVal").textContent = value;
+		getYear(value);
+	}
 });
