5654814d1e9ded033e0e6816820fbb65349017f6
Stephan Kerscher <stephan.kerscher@stud.uni-regensburg.de>
2017-07-24 14:47:52 +0200

    Demo

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 /**
  * Created by Dr.Legendaddy on 24.07.2017.
  */
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    response.location("/index.html");
+    response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'))
-});
-app.get('/', function(req, res) {
-    res.render('index', { title: 'The index page!' })
+    console.log('Node app is running on port', app.get('port'));
 });
