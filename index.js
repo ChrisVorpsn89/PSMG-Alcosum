d04554a3759d399b5ec53693f00c4e10cf644ef9
Stephan Kerscher <stephan.kerscher@stud.uni-regensburg.de>
2017-07-24 14:41:12 +0200

    Demo

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
     response.location("/index.html");
-})
+});
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
 
 app.get('/', function(req, res) {
     res.render('index', { title: 'The index page!' })
