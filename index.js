4c13680ac1af14c744637664b8e8fe495b9bd31c
Stephan Kerscher <stephan.kerscher@stud.uni-regensburg.de>
2017-07-24 14:53:56 +0200

    Demo

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 /**
  * Created by Dr.Legendaddy on 24.07.2017.
  */
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-    response.render('index');
-});
+    response.location("/index.html");
+})
 
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
